e8f6092fea434ec920fccf23e8678c9ca2f5c939
Luis Chilo <gfxargentina@gmail.com>
2021-08-06 11:18:37 -0300

    object manipulation

--- 05-objects.js
+++ 05-objects.js
@@ -33,32 +33,76 @@
 // Juan.saludar();
 
 //extender una subclase desde una clase original
-class Animal {
-    constructor(name, isDomestic) {
-        this.name = name;
-        this.isDomestic = isDomestic;
-        }
-        speak() {
-            console.log(`${this.name} hace un ruido`);
-        }
-}
+// class Animal {
+//     constructor(name, isDomestic) {
+//         this.name = name;
+//         this.isDomestic = isDomestic;
+//         }
+//         speak() {
+//             console.log(`${this.name} hace un ruido`);
+//         }
+// }
 
-class Dog extends Animal {
-    constructor(name, age, isDomestic) {
-        //trae el constructor de la clase padre
-        super(name, isDomestic);
-        this.age = age;        
-    }
+// class Dog extends Animal {
+//     constructor(name, age, isDomestic) {
+//         //trae el constructor de la clase padre
+//         super(name, isDomestic);
+//         this.age = age;        
+//     }
 
-    //metodos, son funciones
-    speak(){
-        console.log(`${this.name} ladra y ${this.isDomestic ? "es domestico" : "es salvaje"}`)
-    }
+//     //metodos, son funciones
+//     speak(){
+//         console.log(`${this.name} ladra y ${this.isDomestic ? "es domestico" : "es salvaje"}`)
+//     }
 
-    sayAge(){
-        console.log(`${this.name} tiene ${this.age}`);
-    }
-}
+//     sayAge(){
+//         console.log(`${this.name} tiene ${this.age}`);
+//     }
+// }
 
-const dog = new Dog("Rulo", 5, true);
-dog.speak();
+// const dog = new Dog("Rulo", 5, true);
+// dog.speak();
+
+//MANIPULACION DE OBJETOS
+const person = {
+    firstName: "Luis",
+    city: "tucuman",
+    job: "Developer",
+};
+
+//acceder a un elemento
+//console.log(person.city);
+
+//modificar un elemento
+//person.city = "Salta";
+//console.log(person.city);
+
+//Object.key() - devuelve un array de las key de un objeto
+// const keys = Object.keys(person);
+// //console.log(keys);
+// //iterar las key de un objeto
+// //keys.forEach((key) => console.log(person[key]));
+
+// for (const key in person) {
+//     if (Object.hasOwnProperty.call(person, key)) {
+//         console.log(person[key]);        
+//     }
+// }
+
+//Object.values -devuelve un array de los valores del objeto
+// const values = Object.values(person);
+// console.log(values);
+// //ahora podemos iterar el array
+// values.forEach((value) => console.log(value));
+
+//Object.assign() - permite asignar los valores de un objeto a otro objeto
+// const developer = {
+//     skills: "Javascript"
+// };
+
+// Object.assign(person, developer);
+// console.log(person);
+
+//Object.entries() - devuelve una matriz(arrays dentro de otro arrays) con una estructura de key values por cada elemento del objeto
+const entries = Object.entries(person);
+console.log(entries);
