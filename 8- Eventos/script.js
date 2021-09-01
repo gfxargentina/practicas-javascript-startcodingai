3bb6b66e0fd6cb04238923ca6b82c32647315296
Luis Chilo <gfxargentina@gmail.com>
2021-09-01 19:24:27 -0300

    8 events intro

---
+++ 8- Eventos/script.js
@@ -0,0 +1,17 @@
+//EVENTOS
+
+//Listado de eventos => https://developer.mozilla.org/es/docs/Web/Events
+
+// const logo = document.querySelector(".puzzle-logo");
+
+// const sayHello = () => {
+//   alert("Hola Mundo");
+
+//   logo.removeEventListener("click", sayHello);
+// };
+
+// // logo.onclick = () => sayHello();
+
+// //addEventListener
+// logo.addEventListener("click", sayHello);
+
