b77b0ecc8acd4bc1ec419f678d0a6b426a74be69
Luis Chilo <gfxargentina@gmail.com>
2021-09-01 19:58:27 -0300

    selected color intro

--- 8- Eventos/script.js
+++ 8- Eventos/script.js
@@ -15,3 +15,13 @@
 // //addEventListener
 // logo.addEventListener("click", sayHello);
 
+const selectColor = (event) => {
+  console.log(event.target.id);
+}
+
+//selecciona todos los elementos con la clase color
+const colorSquares = document.querySelectorAll(".color");
+
+//itera por cada uno de los elementos y le agrega un addEventListener que escuche el click
+//si se produce el click dispara la funcion selectColor
+colorSquares.forEach((square) => square.addEventListener("click", selectColor));
